66faabfc93db2be152bd232f314f7ae4d353b258
Mohit Dhingra <mohitdhingras@gmail.com>
2021-12-06 10:52:28 -0500

    Today's test commit

--- myfunc.c
+++ myfunc.c
@@ -3,7 +3,8 @@
 
 int main()
 {
-    int x = 5, y =2;
+    int x = 5;
+    int y =2;
     printf("%d power %d is %ld\n", x, y, MyPower(x,y));
     printf("%d factorial is %ld\n", x, MyFactorial(x));
     printf("%d is %0.2f%% of %d\n", y, MyPercentage(y, x), x);
